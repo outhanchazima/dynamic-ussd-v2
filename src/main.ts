74ec4d010b9498686793e25b4251db875ebcab8b
Outhan Chazima <outhan254@gmail.com>
2023-02-08 12:23:54 +0300

    main.ts logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  await Logger.log(
+    `*** Application running in on port 3000 *** http://localhost:3000/`,
+  );
 }
 bootstrap();
