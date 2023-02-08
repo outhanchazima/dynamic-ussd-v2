7b267946222c0df7fb46f9e95ce8aecbdb5f0074
Outhan Chazima <outhan254@gmail.com>
2023-02-08 15:50:59 +0300

    configuration and controller setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
 
-@Controller()
+@Controller('ussd')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private config = require('/home/outhan/my_code/dynamic-ussd/config.json');
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  handleUssdRequest(@Req() req: Request, @Res() res: Response) {
+    const { msisdn, sessionid, inputs } = req.query;
+
+    console.log('msisdn: ', msisdn);
+    console.log('sessionId: ', sessionid);
+    console.log('inputs: ', inputs);
+
+    const inputArray = String(inputs).split('*');
+    let currentPage = 'home';
+    let response = '';
+
+    console.log('inputArray: ', inputArray);
+
+    for (const input of inputArray) {
+      const page = this.config[currentPage];
+      response += page.text + '\n';
+
+      if (page.options.length > 0) {
+        for (const [index, option] of page.options.entries()) {
+          response += `${index + 1}. ${option}\n`;
+        }
+        currentPage = page.next[input];
+      } else {
+        // this is an input page, process the input and move to the next page
+        // ...
+        currentPage = page.next.input;
+      }
+    }
+
+    return res.contentType('text/plain').send(response);
   }
 }
