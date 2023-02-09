29110b077de17192a7e97f5fee6053bd33748be2
Outhan Chazima <outhan254@gmail.com>
2023-02-09 14:26:57 +0300

    session provider remove a session method

--- src/providers/session,provider.ts
+++ src/providers/session,provider.ts
@@ -4,7 +4,7 @@ import { Cache } from 'cache-manager';
 export class SessionProvider {
   TTL = 300000; // 5 minutes cache expiry
 
-  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) { }
 
   public async getSessionState(sessionid: string): Promise<string> {
     return await this.cacheManager.get(sessionid);
@@ -19,5 +19,9 @@ export class SessionProvider {
 
   public async getStateData(state: string, config: object): Promise<any> {
     return await config[state];
+  }
+
+  public async clearSessionState(sessionid: string): Promise<void> {
+    return await this.cacheManager.del(sessionid);
   }
 }
